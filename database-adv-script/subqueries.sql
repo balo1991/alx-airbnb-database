acbfc9391916a98e175d4c2ffef2b3164c6c76b2
Maruf Balogun <marufbyte@gmail.com>
2025-09-05 12:14:20 +0100

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,30 @@
+-- 1. Non-correlated subquery:
+-- Find all properties where the average rating is greater than 4.0
+SELECT 
+    p.id AS property_id,
+    p.title,
+    p.location
+FROM properties p
+WHERE p.id IN (
+    SELECT r.property_id
+    FROM reviews r
+    GROUP BY r.property_id
+    HAVING AVG(r.rating) > 4.0
+)
+ORDER BY p.id;
+
+
+-- 2. Correlated subquery:
+-- Find users who have made more than 3 bookings
+SELECT 
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.id
+) > 3
+ORDER BY u.id;
